77fd43afea447734d974942633bef2236429f1ba
scngnr <scngnr@gmail.com>
2022-09-07 19:41:03 +0300

    4

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,41 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        ini_set ( 'max_execution_time', -1);
+
+        // @see home_directory_config.php
+        // use config from ~/.confg/jaggedsoft/php-binance-api.json
+        $api = new BinanceClient('IUSTUlCe8f74A4F0gmx0OqXlbe3ZKDu7wg0eI6WQNxbzv1EAK8QgV8F4zhr1EpBe', 'yO2rd9C88FQXaH6L7hbXvmksLwspYKbLrE5ACy2vzEz41v1CySPJ3RpGVjHcCU4I');
+
+        $market = $api->Margin->marketPrice();
+        // echo "<pre>";
+        // print_r($market);
+
+        for ($i=0; $i < count($market); $i++) {
+
+          $checkCrypto = Crypto::where('symbol', $market[$i]['symbol'])->first();
+          if($checkCrypto){
+
+            $crypto = Crypto::find($checkCrypto->id);
+            $crypto->symbol                 = $market[$i]['symbol'];
+            $crypto->markPrice              = $market[$i]['markPrice'];
+            $crypto->indexPrice             = $market[$i]['indexPrice'];
+            $crypto->estimatedSettlePrice   = $market[$i]['estimatedSettlePrice'];
+            $crypto->lastFundingRate        = $market[$i]['lastFundingRate'];
+            $crypto->nextFundingTime        = $market[$i]['nextFundingTime'];
+            $crypto->update();
+          }else {
+
+            $crypto                         = new  Crypto;
+            $crypto->symbol                 = $market[$i]['symbol'];
+            $crypto->markPrice              = $market[$i]['markPrice'];
+            $crypto->indexPrice             = $market[$i]['indexPrice'];
+            $crypto->estimatedSettlePrice   = $market[$i]['estimatedSettlePrice'];
+            $crypto->lastFundingRate        = $market[$i]['lastFundingRate'];
+            $crypto->nextFundingTime        = $market[$i]['nextFundingTime'];
+            $crypto->save();
+          }
+        }
     }
 
     /**
